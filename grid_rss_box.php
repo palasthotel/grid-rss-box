0da9c1d28b745b7a0de96aa0bcdbfb039a0f8cbd
EdwardBock <edward.bock@palasthotel.de>
2016-06-07 11:48:52 +0200

    nice templates

--- grid_rss_box.php
+++ grid_rss_box.php
@@ -20,47 +20,46 @@ class grid_rss_box extends grid_list_box
 
   public function build($editmode)
   {
-	if($editmode)
+	if(isset($this->content->url) && $this->content->url!="")
 	{
-	  return t("RSS Feed");
-	}
-	else
-	{
-	  if(isset($this->content->url) && $this->content->url!="")
+	  $pie=new SimplePie();
+	  $pie->set_cache_location(self::$CACHE_DIR);
+	  if(!file_exists(self::$CACHE_DIR))
 	  {
-		$pie=new SimplePie();
-		$pie->set_cache_location(self::$CACHE_DIR);
-		if(!file_exists(self::$CACHE_DIR))
-		{
-		  mkdir(self::$CACHE_DIR);
-		}
-
-		$pie->set_feed_url($this->content->url);
-		$pie->set_item_limit($this->content->numItems);
-
-		$pie->init();
-		$this->feed=$pie;
-
-		$i=0;
-		$items = array();
-
-
-		foreach($pie->get_items(0,$this->content->numItems) as $item)
-		{
-
-		  $_item = new grid_rss_box_item($item);
-
-		  if($i==0) $_item->addClass("grid-rss-item-first");
-		  if($i==$this->content->numItems-1) $_item->addClass("grid-rss-item-last");
-
-		  $items[] = $_item;
-
-		}
-
-		return $items;
+		mkdir(self::$CACHE_DIR);
 	  }
-	  return "";
+
+	  $pie->set_feed_url($this->content->url);
+	  $pie->set_item_limit($this->content->numItems);
+
+	  $pie->init();
+	  $this->feed=$pie;
+
+	  $i=$this->content->numItems;
+	  $items = array();
+
+	  foreach($pie->get_items(0,$this->content->numItems) as $item)
+	  {
+
+		$_item = new grid_rss_box_item($item);
+
+		if($i==0) $_item->addClass("grid-rss-item-first");
+		if($i==$this->content->numItems-1) $_item->addClass("grid-rss-item-last");
+
+		$items[] = $_item;
+
+		/*
+		 * stop it on max items
+		 */
+		$i--;
+		if($i < 0) break;
+
+	  }
+
+	  return $items;
 	}
+	return t("RSS Feed");
+
   }
 
   public function contentStructure() {
--- templates/grid-box-rss.tpl.php
+++ templates/grid-box-rss.tpl.php
@@ -1,19 +1,55 @@
 
-<ul class="grid-rss-items">
+<div class="grid-box<?php echo ($this->style)? " ".$this->style." ": " "; echo implode($this->classes," ")?>">
   <?php
-  foreach($content as $item){
-	/**
-	 * @var grid_rss_box_item $item
-	 */
+  if ($this->title!=""){
+
+	if ($this->titleurl !=""){
+	  ?>
+	  <h2 class="grid-box-title b-title"><a href="<?php echo $this->titleurl?>"><?php echo $this->title?></a></h2>
+	<?php }else{?>
+	  <h2 class="grid-box-title b-title"><?php echo $this->title?></h2>
+	<?php }?>
+  <?php }?>
+
+  <?php if($this->prolog != "") { ?>
+	<div class="grid-box-prolog b-prolog">
+	  <?php echo $this->prolog?>
+	</div>
+  <?php } ?>
+
+  <ul class="grid-rss-items">
+	<?php
+	foreach($content as $item){
+	  /**
+	   * @var grid_rss_box_item $item
+	   */
+	  ?>
+	  <li class="<?php echo implode(" ", $item->getClasses()); ?>"><a href="<?php
+		echo $item->getPermalink();
+		?>"><?php
+		  echo $item->getDate("d.M.Y - H:i")."<br>";
+		  echo $item->getTitle()."<br>";
+		  echo $item->getDescription()
+		  ?></a></li>
+	  <?php
+	}
 	?>
-	<li class="<?php echo implode(" ", $item->getClasses()); ?>"><a href="<?php
-	  	echo $item->getPermalink();
-	  ?>"><?php
-	  	echo $item->getDate("d.M.Y - H:i")."<br>";
-		echo $item->getTitle()."<br>";
-		echo $item->getDescription()
-	  ?></a></li>
+  </ul>
+
+  <?php if($this->epilog != ""){ ?>
+	<div class="grid-box-epilog b-epilog">
+	  <?php echo $this->epilog?>
+	</div>
+  <?php } ?>
+
   <?php
-  }
-  ?>
-</ul>
+  if ($this->readmore!=""){?>
+	<a href="<?php echo $this->readmoreurl?>" class="grid-box-readmore-link b-readmore-link"><?php echo $this->readmore?></a>
+  <?php }?>
+
+</div>
+
+
+
+
+
